ebf7060204fccddee065921d4b076fa959f3e78d
Firmin.Sun <fmsunyh@gmail.com>
2024-07-18 16:23:24 +0800

    new logo

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 Author: Firmin.Sun fmsunyh@gmail.com
 Date: 2024-07-05 22:11:11
 LastEditors: Firmin.Sun fmsunyh@gmail.com
-LastEditTime: 2024-07-18 14:49:06
+LastEditTime: 2024-07-18 15:36:57
 FilePath: \aistore\version.py
 Description: 
 '''
