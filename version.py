02479096ee89c47749ed2291f20490360d4361d9
Firmin.Sun <fmsunyh@gmail.com>
2024-07-31 18:21:05 +0800

    release 1.4.7

--- version.py
+++ version.py
@@ -2,9 +2,9 @@
 Author: Firmin.Sun fmsunyh@gmail.com
 Date: 2024-07-05 22:11:11
 LastEditors: Firmin.Sun fmsunyh@gmail.com
-LastEditTime: 2024-07-31 17:42:55
+LastEditTime: 2024-07-31 18:20:48
 FilePath: \aistore\version.py
 Description: 
 '''
-__version__ = "1.4.6"
+__version__ = "1.4.7"
 __author__ = "ZhongJu GPU Cloud"
